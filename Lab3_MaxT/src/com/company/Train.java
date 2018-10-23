83b98e931512c47a02636ffe590cd205f4c7739a
Maxim-Turovets <36541363+Maxim-Turovets@users.noreply.github.com>
2018-10-23 21:07:55 +0300

    add seter and geter

--- Lab3_MaxT/src/com/company/Train.java
+++ Lab3_MaxT/src/com/company/Train.java
@@ -10,8 +10,59 @@ public class Train {
     private  int NumberPlz;
     private  int NumberSv;
 
+    public void setDestination(String destination) {
+        Destination = destination;
+    }
 
+    public void setNumberCommon(int numberCommon) {
+        NumberCommon = numberCommon;
+    }
 
+    public void setNumberKoupe(int numberKoupe) {
+        NumberKoupe = numberKoupe;
+    }
 
+    public void setNumberPlz(int numberPlz) {
+        NumberPlz = numberPlz;
+    }
 
+    public void setNumberTrain(int numberTrain) {
+        NumberTrain = numberTrain;
+    }
+
+    public void setNumberSv(int numberSv) {
+        NumberSv = numberSv;
+    }
+
+    public void setSendTime(double sendTime) {
+        SendTime = sendTime;
+    }
+
+    public double getSendTime() {
+        return SendTime;
+    }
+
+    public int getNumberCommon() {
+        return NumberCommon;
+    }
+
+    public int getNumberKoupe() {
+        return NumberKoupe;
+    }
+
+    public int getNumberPlz() {
+        return NumberPlz;
+    }
+
+    public int getNumberSv() {
+        return NumberSv;
+    }
+
+    public int getNumberTrain() {
+        return NumberTrain;
+    }
+
+    public String getDestination() {
+        return Destination;
+    }
 }
