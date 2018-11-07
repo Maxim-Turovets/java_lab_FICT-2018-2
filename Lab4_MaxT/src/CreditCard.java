2b334e6b5a705dbf57ca43c542215735c17f4862
Maxim-Turovets <36541363+Maxim-Turovets@users.noreply.github.com>
2018-11-07 18:50:13 +0200

    add code

--- Lab4_MaxT/src/CreditCard.java
+++ Lab4_MaxT/src/CreditCard.java
@@ -1,12 +1,45 @@
 public class CreditCard {
 
-    private int number;
+    private  int number;
     private  double balance;
     private  boolean active;
 
     CreditCard (int number, double balance, boolean aсtive)
     {
-        
+     this.number=number;
+     this.balance=balance;
+     this.active = active;
     }
 
+    CreditCard ()
+    {
+        this.number=1111;
+        this.balance=9999;
+        this.active = true;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public  boolean getActive()
+    {
+        return active;
+    }
 }
---
+++ Lab4_MaxT/src/UserOperation.java
@@ -0,0 +1,10 @@
+public  class UserOperation {
+
+
+
+ public  static void BlocCard(User user){
+     user.getCreditCard().setActive(false);
+ }
+
+ public  static  void 
+}
