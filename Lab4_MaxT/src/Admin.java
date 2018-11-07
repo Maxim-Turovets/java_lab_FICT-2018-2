14bb3f5806d1b5b7baffa15ff5556fff0291aa1f
Maxim-Turovets <36541363+Maxim-Turovets@users.noreply.github.com>
2018-11-08 01:07:06 +0200

    fix

--- Lab4_MaxT/src/Admin.java
+++ Lab4_MaxT/src/Admin.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Admin extends Person {
 
-   Admin(){
-       Scanner in = new Scanner(System.in);
-       System.out.println("Введите логин");
-       setName(in.nextLine());
-   }
+//   Admin(){
+//       Scanner in = new Scanner(System.in);
+//       System.out.println("Введите логин");
+//       setName(in.nextLine());
+//   }
 }
